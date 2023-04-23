29b70798c8545b074ca4947262a245722713a147
juhiesayyed <86836506+juhiesayyed@users.noreply.github.com>
2023-04-23 13:06:47 +0530

    Create highlight.js

---
+++ Frontend/src/components/Home/Highlight/highlight.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./highlight.css"
+
+const highlight = () => {
+    return (
+    <div className='outerbox'>
+    <div className='Latest-Updates'>
+    The "10 + 2" structure will be replaced with "5+3+3+4" model. This will be implemented as follows: Foundational Stage: This is further subdivided into two parts: 3 years of preschool or anganwadi, followed by classes 1 and 2 in primary school. This will cover children of ages 3–8 years.
+    </div>
+    <div className='Small-Rectangle'>
+    Latest Updates
+    </div>
+    </div>
+    )
+}
+
+export default highlight
