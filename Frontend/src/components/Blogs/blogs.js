1b88326e2b9b40489f2c8b27d563659d085d314f
juhiesayyed <86836506+juhiesayyed@users.noreply.github.com>
2023-04-23 12:51:21 +0530

    Create blogs.js

---
+++ Frontend/src/components/Blogs/blogs.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import { Link } from "react-router-dom"
+import Nav from "../Nav/nav"
+// import "../Nav/nav.css"
+const blogs = () => {
+  return (
+    <>
+      <Nav />
+      <div>Blogs</div>
+    </>
+  )
+}
+
+export default blogs
