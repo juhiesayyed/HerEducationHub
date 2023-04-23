264c52a8ae0e1bf6ead50e1251644c37f113e325
juhiesayyed <86836506+juhiesayyed@users.noreply.github.com>
2023-04-23 12:53:39 +0530

    Create contact.js

---
+++ Frontend/src/components/Contact/contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./contact.css"
+
+const contact = () => {
+    return (
+        <div className='footer' id='Contact'>
+
+        </div>
+    )
+}
+
+export default contact
